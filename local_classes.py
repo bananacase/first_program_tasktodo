fb6a294546f0738dfcf00bffa747a37df3a27114
bananacase <pprivoda@mail.ru>
2019-12-14 04:34:43 +0300

    initial commit with Day class

---
+++ local_classes.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+'''
+There are several classes built for Task Managers project.
+These classes could be used as backend data collectors in further applications.
+Classes:
+    Day: interactive data structure, that stores tasks for any day.
+'''
+import pickle
+import os
+
+a = os.sep
+
+
+class Day:
+    """
+    This class is needed to store data about a specific day. 
+    Day is a class object. The __init__ method accepts today's as a 'date'
+    and the global prefix as a 'source'.
+    This class performs the following methods:
+    1. Creating new Tasks in different parts of the data strusture
+    2. The ability to integrate data from one previous object into the current
+    3. The ability to change the status of the Task
+    4. Saving the data strusture of the weaving object to the directory
+    """
+
+    days_per_month = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+
+    def __init__(self, date: str, source='') -> None:
+        '''
+        date: str 'YYYY-MM-DD' (as same as GNU format)
+        source: str matches '^/.*/$'
+        '''
+        self.source = source
+        self.today_do = {0: ['Survive', True]}
+        self.tomorrow_do = {0: ['Survive', True]}
+        self.insite_day = ''
+        self.date = date.split('-')
+
+        for i in self.date[:-1]:
+            source += (i+'/')
+            try:
+                os.mkdir(source)
+            except FileExistsError:
+                pass
+        source += self.date[-1]
+        self.path = source
+
+    def input_today_do(self, text: str) -> None:
+        '''
+        This method creating to the new one Task in this block with parameter 'text'
+        This block named: 'Today'
+        The parameter 'text': str
+        '''
+        self.today_do[list(self.today_do.keys())[-1]+1] = [text, False]
+
+    def input_tomorrow_do(self, text: str) -> None:
+        '''
+        This method creating to the new one Task in this block with parameter 'text'
+        This block named: 'Tomorrow'
+        The parameter 'text': str
+        '''
+        self.tomorrow_do[list(self.tomorrow_do.keys())[-1]+1] = [text, False]
+
+    def podsos(self) -> None:
+        '''
+        podsos - it is the best method which changed ours life. It is perfect, truly. Seriously:
+        This method integrates data from one previous object into the current
+        '''
+        year_old = int(self.date[0])
+        month_old = int(self.date[1])
+        day_old = int(self.date[2])
+
+        if day_old == 1:
+            if month_old == 1:
+                year_old -= 1
+                month_old = 12
+                day_old = 31
+            else:
+                month_old -= 1
+                day_old = Day.days_per_month[month_old]
+        else:
+            day_old -= 1
+
+        old_source = self.source + f'{str(year_old)}/{str(month_old)}/{str(day_old)}'
+
+        try:
+            with open(old_source, 'rb') as f:
+                data_old = pickle.load(f)
+        except FileNotFoundError:
+            pass
+
+        for i, value in data_old.tomorrow_do.items():
+            if i != 0:
+                self.input_today_do(value[0])
+
+    def changest(self, id: int) -> None:
+        '''
+        This method changes the status ot the Task
+        '''
+        self.today_do[id][1] = not self.today_do[id][1]
+
+    def dump(self) -> None:
+        '''
+        This method saves the data strusture of the weaving object to the hard drive
+        '''
+        with open(self.path, 'wb') as f:
+            pickle.dump(self, f)
